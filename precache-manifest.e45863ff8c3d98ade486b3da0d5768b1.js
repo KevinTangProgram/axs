07386b5e55e62de424b4da2004c3be486cef7444
KevinTangProgram <121833798+KevinTangProgram@users.noreply.github.com>
2023-01-21 00:03:00 -0800

    Add files via upload

---
+++ precache-manifest.e45863ff8c3d98ade486b3da0d5768b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "04ef02c7a28b79350a83",
+    "url": "/axs/static/js/main.04ef02c7.chunk.js"
+  },
+  {
+    "revision": "6fd8969915401c84bb67",
+    "url": "/axs/static/js/1.6fd89699.chunk.js"
+  },
+  {
+    "revision": "e0c6cc3054b7e7224b8a",
+    "url": "/axs/static/js/runtime~main.e0c6cc30.js"
+  },
+  {
+    "revision": "2f6f748d156dfc3a21ecd77efac62847",
+    "url": "/axs/index.html"
+  }
+];
